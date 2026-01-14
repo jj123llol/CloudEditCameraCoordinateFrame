57d6a4fe149418e5828021796ce6ef0f5ee8e6e2
really_ant <115508192+jj123llol@users.noreply.github.com>
2026-01-13 19:19:41 -0600

    Update src.lua

--- src.lua
+++ src.lua
@@ -243,7 +243,7 @@ mod['Listen'] = function(channel : number)
         local cf = CFrame.new(channel, num1, num2)
         mod.cloudcf["set"](cf)
         --task.wait(.15)
-		game:GetService("RunService").RenderStepped:Wait()
+		game:GetService("RunService").Heartbeat:Wait()
         mod.cloudcf["set"](og)
     end
 
